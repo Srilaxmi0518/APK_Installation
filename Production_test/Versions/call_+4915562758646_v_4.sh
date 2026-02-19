c11dde690f7ec11777ad56b391da70a9cf43674c
srilaxmibutukuri@gmail.com <Srilaxmi@509>
2026-02-19 12:13:58 +0100

    Auto-generate scripts (3 files)

---
+++ Production_test/Versions/call_+4915562758646_v_4.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Generated on 2026-02-19T11:13:57.945487 UTC
+
+export MT="+491793934532"
+
+export ENABLE_VOICE=true
+export VOICE_DURATION=40
+
+export ENABLE_SMS=true
+export ENABLE_MMS=true
+export ENABLE_DATA=true
+
+exec "$(dirname "$0")/../base.sh"
